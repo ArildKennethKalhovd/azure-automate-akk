89093ce2ad31518d975b23f57ba1048e1e0f26f2
Arild-Kenneth Kalhovd <arild-kenneth.kalhovd@nav.no>
2022-05-04 13:23:29 +0200

    Renset for kommentarer

---
+++ src/oppgave_3.ps1
@@ -0,0 +1,32 @@
+#Tatt bort all kommentering fra hint-filen
+
+$ErrorActionPreference = 'Stop'
+
+$webRequest = Invoke-WebRequest -Uri "http://nav-deckofcards.herokuapp.com/shuffle"
+
+$kortstokkJson = $webRequest.Content
+
+$kortstokk = ConvertFrom-Json -InputObject $kortstokkJson
+
+foreach ($kort in $kortstokk) {
+    Write-Output $kort
+}
+
+foreach ($kort in $kortstokk) {
+    Write-Output "$($kort.suit[0])+$($kort.value)"
+}
+
+function kortstokkTilStreng {
+    [OutputType([string])]
+    param (
+        [object[]]
+        $kortstokk
+    )
+    $streng = ""
+    foreach ($kort in $kortstokk) {
+        $streng = $streng + "$($kort.suit[0])" + "$($kort.value)" + ","
+    }
+    return $streng
+}
+
+Write-Output "Kortstokk: $(kortStokkTilStreng -kortstokk $kortstokk)"
